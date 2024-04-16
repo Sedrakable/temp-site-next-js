fc2afec56fe2db27a41fecef09e1875905b9ce60
Sedrakable <sedrak005@gmail.com>
2024-04-16 14:08:45 -0400

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"daebe2d0122514b087afbfe9ad74f0bde3534870\":{\"workers\":{\"app/page\":\"52561\"},\"layer\":{\"app/page\":\"action-browser\"}}},\"edge\":{},\"encryptionKey\":\"D36LJIN8h8DwqO26lg/iC8BX1bRQ5u/HEy9Z6sacpdA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"daebe2d0122514b087afbfe9ad74f0bde3534870\":{\"workers\":{\"app/page\":\"52561\"},\"layer\":{\"app/page\":\"action-browser\"}}},\"edge\":{},\"encryptionKey\":\"WG0yrU7NuoU++z/iYGgOoAUJu+vaipJQzpG9FhihEwk=\"}"
